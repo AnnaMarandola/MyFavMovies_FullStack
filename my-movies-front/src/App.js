406698ed9aadbac79a920414aeb41118d6ba6493
AnnaMarandola <annamarandola888@gmail.com>
2019-12-14 23:26:14 +0100

    creation du composent MovieItem

--- my-movies-front/src/App.js
+++ my-movies-front/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import MovieItem from './components/MovieItem';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MovieItem 
+        title="Kill Bill"
+        genre="Action"
+        rate="5"
+        director="Quentin Tarentino"
+        released="2003-11-26"
+        sum_up="After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her."
+        poster_url="https://m.media-amazon.com/images/M/MV5BNzM3NDFhYTAtYmU5Mi00NGRmLTljYjgtMDkyODQ4MjNkMGY2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+  export default App;
---
+++ my-movies-front/src/components/MovieItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MovieItem = ({title, director, released, poster_url, rate, sum_up, genre, isUnder16}) => (
+    <div className="MovieItem">
+        <img alt={title} src={poster_url}/>
+        <div>
+            <h1>{title}</h1>
+            <h3>Genre:  {genre}</h3>
+            <h4>Director: {director}</h4>
+            <h4> {released}</h4>
+            <p>{sum_up}</p>
+            <h4>rating: {rate}</h4>
+            </div>
+    </div>
+);
+
+export default MovieItem;
