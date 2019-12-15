4c0b47c0fa759d5452d6ba33c890772898a638fe
AnnaMarandola <annamarandola888@gmail.com>
2019-12-15 17:36:23 +0100

    create a route POST to add new movie to db

--- back/index.js
+++ back/index.js
@@ -1,22 +1,11 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
+app.use(bodyParser.json());
 
-// const movies = [
-//     { 
-//       id: 1,
-//       title: 'Kill Bill',
-//       genre:'Action',
-//       rate:'5',
-//       director:'Quentin Tarentino',
-//       released:'2003-11-26',
-//       sum_up:'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.',
-//       poster_url: 'https://m.media-amazon.com/images/M/MV5BNzM3NDFhYTAtYmU5Mi00NGRmLTljYjgtMDkyODQ4MjNkMGY2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'
-  
-  
-//     }
-//   ]
+
 
 app.get('/api/movies', (req, res) => {
     db.query('SELECT * FROM myMovies', (err, movies) => {
@@ -24,7 +13,14 @@ app.get('/api/movies', (req, res) => {
     })
 });
 
-
+app.post('/api/movies', (req, res) => {
+    db.query('INSERT INTO myMovies SET ?', req.body, (err, results) => {
+        if(err) {
+            return res.status(500).send(err.message);
+        }
+        res.json(results);
+    })
+})
 
 
 app.listen(8000);
---
+++ back/sql/init.sql
@@ -0,0 +1,10 @@
+{
+"title": "Rendez-moi  ma peau",
+"director": "Patrick Schulmann",
+"released": "1980-10-05",
+"isUnder16": "",
+"poster_url": "https://2.bp.blogspot.com/-yqWWUr43EIc/WNptE6i6YrI/AAAAAAAAQdI/SV7NrINtViYVnEG0uwyuvCfpI0PPpgivQCLcB/s1600/09.jpg",
+"rate": 4,
+"sum_up": "À la suite d'un accident de voiture, Zora, une sorcière ne maîtrisant plus ses pouvoirs, intervertit de colère les corps d'un homme et d'une femme impliqués dans l'accident. S'ensuit une multitude de situations cocasses où les trois larrons vont tout faire pour que Zora récupère ses pleins pouvoirs et que les ensorcelés retrouvent leurs apparences respectives.",
+"genre": "Comedy"
+}
