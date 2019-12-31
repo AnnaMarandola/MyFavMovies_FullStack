653100282fe9cbee701ae477f88ebb6313bf6725
AnnaMarandola <annamarandola888@gmail.com>
2019-12-31 13:34:39 +0100

    route get by genre

--- back/index.js
+++ back/index.js
@@ -8,12 +8,29 @@ app.use(bodyParser.json());
 
 
 app.get('/api/movies', (req, res) => {
-    db.query('SELECT * FROM myMovies', (err, movies) => {
-        res.json(movies);
-    })
+  let sql = 'SELECT * FROM myMovies';
+  const sqlValues = [];
+
+  if (req.query.rating) {
+    sql += ' WHERE rating =?';
+    sqlValues.push(req.query.rating);
+  }
+
+  if (req.query.genre) {
+    sql += ' WHERE genre =?';
+    sqlValues.push(req.query.genre);
+  }
+
+  db.query(sql, sqlValues, (err, results) => {
+    if (err) {
+      res.status(500).send(`An error occurred: ${err.message}`);
+    } else {
+      res.json(results);
+    }
+  });
 });
 
-app.post('/api/movies', (req, res) => {
+app.post('/api/movie', (req, res) => {
     db.query('INSERT INTO myMovies SET ?', req.body, (err, results) => {
         if(err) {
             return res.status(500).send(err.message);
